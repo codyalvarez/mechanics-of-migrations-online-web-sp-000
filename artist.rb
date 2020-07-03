6cad3c3fb3ce2033fa8b259edc40fcf14c2edf53
Cody Alvarez <cody_alvarez19@outlook.com>
2020-07-03 19:20:19 +0000

    Automatically backed up by Learn

--- artist.rb
+++ artist.rb
@@ -1,10 +1,3 @@
 class Artist < ActiveRecord::Base
-  attr_accessor :name, :genre, :age, :hometown
 
-  def initialize
-    @name = name
-    @genre = genre
-    @age = age
-    @hometown = hometown
-  end
 end
